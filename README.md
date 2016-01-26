5e2a3e575da4e184d8051465a040006f1317900f
tamerk <tamer_kulaksizoglu@yahoo.com>
2016-01-26 18:23:29 +0200

    Added Main class

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Java-StRD ##
-A project to check the numerical accuracy of Java libraries against the Statistical Reference Datasets
+This project aims to check the accuracy of Java numerical libraries against the [Statistical Reference Datasets](http://www.itl.nist.gov/div898/strd/).
 
 ## Libraries ##
 - [Apache Commons Mathematics Library 3.6](http://commons.apache.org/proper/commons-math/)
@@ -7,3 +7,9 @@ A project to check the numerical accuracy of Java libraries against the Statisti
 ## Datasets ##
 
 ### Nonlinear Regression ###
+
+<pre>
+| Dataset| GN1     | GN2     | LM1     | LM2     |
+|================================================|
+| Misra1a|    11.13|    11.13|     9.78|    10.13|
+</pre>
---
+++ src/strd/acm/nr/Main.java
@@ -0,0 +1,67 @@
+package strd.acm.nr;
+
+import dnl.utils.text.table.TextTable;
+import java.util.ArrayList;
+import java.util.List;
+import strd.data.nr.*;
+
+public class Main {
+
+    public static void test(NonlinearRegressionModel model,
+            NonlinearRegressionDataset dataset, List<String> listRows) {
+        model.setMaximumEvaluations(5000);
+        model.setMaximumIterations(5000);
+        model.setPredictorVariables(dataset.getPredictorVariables());
+        model.setResponseVariable(dataset.getResponseVariable());
+        model.setTolerance(1e-15);
+        double[] calculated, certified;
+        double logOfRelativeError;
+        listRows.add(dataset.getDatasetName());
+
+        model.setOptimizer(Optimizer.GaussNewton);
+        certified = dataset.getParameterValues();
+
+        model.estimate(dataset.getStartingValues1());
+        calculated = model.getParameterValues();
+        logOfRelativeError = model.logOfRelativeError(calculated, certified);
+        listRows.add(String.format("%8.2f", logOfRelativeError));
+
+        model.estimate(dataset.getStartingValues2());
+        calculated = model.getParameterValues();
+        logOfRelativeError = model.logOfRelativeError(calculated, certified);
+        listRows.add(String.format("%8.2f", logOfRelativeError));
+
+        model.setOptimizer(Optimizer.LevenbergMarquardt);
+        certified = dataset.getParameterValues();
+
+        model.estimate(dataset.getStartingValues1());
+        calculated = model.getParameterValues();
+        logOfRelativeError = model.logOfRelativeError(calculated, certified);
+        listRows.add(String.format("%8.2f", logOfRelativeError));
+
+        model.estimate(dataset.getStartingValues2());
+        calculated = model.getParameterValues();
+        logOfRelativeError = model.logOfRelativeError(calculated, certified);
+        listRows.add(String.format("%8.2f", logOfRelativeError));
+    }
+
+    public static void main(String[] args) {
+        String[] columnNames = new String[]{"Dataset", "GN1", "GN2", "LM1", "LM2"};
+        Object[] row;
+        Object[][] data = new Object[1][5];
+
+        NonlinearRegressionDataset dataset;
+        NonlinearRegressionModel model;
+        List<String> listRows = new ArrayList<>();
+
+        dataset = new Misra1aDataset();
+        model = new Misra1aModel();
+        test(model, dataset, listRows);
+        row = new Object[5];
+        row = listRows.toArray(row);
+        data[0] = row;
+
+        TextTable textTable = new TextTable(columnNames, data);
+        textTable.printTable();
+    }
+}
